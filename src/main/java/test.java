6c8126f6fcfb165b9e42bed489d2486cae45a4a0
sunwei <2937157944@qq.com>
2021-01-21 11:09:36 +0800

    分支dev初始化操作

--- src/main/java/test.java
+++ src/main/java/test.java
@@ -1,5 +1,7 @@
 public class test {
     public static void main(String[] args) {
-        System.out.println("初始化操作");
+        System.out.println("初始化操作....");
+        System.out.println("dev分支初始化操作.....");
+
     }
 }
