8e77d37505b87e419521ed3c61fde2c1ab135540
sunwei <2937157944@qq.com>
2021-01-21 11:18:15 +0800

    主干初始化操作结束

--- src/main/java/test.java
+++ src/main/java/test.java
@@ -2,6 +2,7 @@ public class test {
     public static void main(String[] args) {
         System.out.println("初始化操作....");
         System.out.println("dev分支初始化操作.....");
+        System.out.println("主干初始化操作.....");
 
     }
 }
