8c77677a083702cdc6a208ad39df082c64fc058d
sunwei <2937157944@qq.com>
2021-01-21 11:16:43 +0800

    分支dev初始化操作结束

--- src/main/java/test.java
+++ src/main/java/test.java
@@ -2,6 +2,8 @@ public class test {
     public static void main(String[] args) {
         System.out.println("初始化操作....");
         System.out.println("dev分支初始化操作.....");
+        System.out.println("dev分支初始化操作结束.....");
+
 
     }
 }
